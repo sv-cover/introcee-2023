51b51ab1b1e36706886f68981fd56989e1f3ee71
Fabian Cuza <fabiancuza@Fabians-MacBook-Pro.local>
2023-09-04 23:16:50 +0200

    fix header insta link

--- resources/views/components/mediapartner.blade.php
+++ resources/views/components/mediapartner.blade.php
@@ -1,7 +1,7 @@
 <div class="media-partner">
     <ul class="media-links clearfix">
         <li><h6>Follow on social media</h6></li>
-        <li><a href="https://instagram.com/svcover.nl/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="https://instagram.com/svcover/" target="_blank"><i class="fab fa-instagram"></i></a></li>
         <li><a href="https://whatsapp.svcover.nl/" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
         <li><a href="https://nl.linkedin.com/company/svcover/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
     </ul>
